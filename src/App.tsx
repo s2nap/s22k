536801d3ee4acb1a41cdc4597f86828e0486eb75
s2nap <s2nap@gmail.com>
2026-04-26 18:18:12 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,148 +1,35 @@
-
-import heroImg from './assets/yungsnap.png'
+import LogoImg from './assets/yungsnap.png'
 import './App.css'
+
+const links = [
+  { name: 'YouTube', user: '@yungsnap', url: 'https://www.youtube.com/@yungsnap' },
+  { name: 'YouTube', user: '@ss2nap', url: 'https://www.youtube.com/@ss2nap' },
+  { name: 'TikTok', user: '@ss2nap', url: 'https://www.tiktok.com/@ss2nap' },
+  { name: 'Instagram', user: '@ss2nap', url: 'https://www.instagram.com/ss2nap' },
+]
 
 export default function App() {
   return (
     <>
+      {/* LOGO */}
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="500" height="100" alt="500x100" />
-          
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-           <code>src/App.tsx and save to test HMR</code>
-          </p>
-        </div>
+        <img src={LogoImg} className="base" alt="logo" />
       </section>
 
-      <div className="ticks"></div>
-
+      {/* LINKS */}
       <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                X
+        <h2>Social Media</h2>
+        <ul className="links">
+          {links.map((link, i) => (
+            <li key={i}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                <span>{link.name}</span>
+                <small>{link.user}</small>
               </a>
             </li>
-
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                X2
-              </a>
-            </li>
-
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X3
-              </a>
-            </li>
-
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X4
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        {/* 2nd column */}
-        
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                X
-              </a>
-            </li>
-
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                X2
-              </a>
-            </li>
-
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X3
-              </a>
-            </li>
-
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true">
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X4
-              </a>
-            </li>
-
-          </ul>
-        </div>
+          ))}
+        </ul>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
