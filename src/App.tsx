425d46794f5e2e1148d76c7919dc2659562ffc93
s2nap <90632157+s2nap@users.noreply.github.com>
2026-09-07 19:59:29 -0600

    Update site design and assets

--- src/App.tsx
+++ src/App.tsx
@@ -14,43 +14,24 @@ const links = [
 export default function App() {
   return (
     <main className="wrapper">
-      
       <div className="card">
-  
-        {/* LOGO */}
         <section id="center">
           <img src={LogoImg} className="base" alt="logo" />
         </section>
 
-        {/* LINKS */}
         <section id="next-steps">
-          <h2>طَقْ</h2>
-
-        <ul className="links">
-          {links.map((link, i) => (
-          <li key={i}>
-            <a href={link.url} target="_blank" rel="noopener noreferrer">
-            <img src={link.icon} className="icon" />
-            <small>{link.user}</small>
-            </a>
-        </li>
-
-        
-      ))}
-       </ul>
-      </section>
-    </div>
-      {/*<div className="card bottom-card">
-  <ul className="bottom-links">
-    {Array.from({ length: 3 }).map((_, i) => (
-      <li key={i}>
-        <a href="#">
-          Botón {i + 1}
-        </a>
-      </li>
-    ))}
-  </ul>
-</div>*/}
+          <ul className="links">
+            {links.map((link, i) => (
+              <li key={i}>
+                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                  <img src={link.icon} className="icon" alt="icon" />
+                  <small>{link.user}</small>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </main>
   )
 }
